7b96ba9fc957e34ff4359e59d5b630468df0ac21
judelang18 <jude.andrade@evsu.edu.ph>
2025-06-16 00:07:06 +0800

    commit

--- python/D9_error_handling.py
+++ python/D9_error_handling.py
@@ -88,3 +88,71 @@
        
 
 #--------------------------------------------------------------
+
+# Error handling using dict instead of match function - Day of the Week Activity
+
+# days_of_the_week = {1: "Sunday", 
+#                     2: "Monday",
+#                     3: "Tuesday",
+#                     4: "Wednesday",
+#                     5: "Thursday",
+#                     6: "Friday",
+#                     7: "Saturday"}
+
+# print("\nLegend:\nSunday = 1\nMonday = 2\nTuesday = 3\nWednesday = 4\nThursday = 5\nFriday = 6\nSaturday = 7\n")
+
+# while True:
+#     try:
+#         day_num_input = int(input("What is the number of the day in a week: "))
+#     except ValueError: 
+#         print("Only input an integer assigned based on the Legend!\n")
+#     else:
+#         if day_num_input == 0:
+#             print("Zero is not a valid representation of a day in a week!\n")
+#             continue
+#         else:
+#             break
+        
+# print(f"Today is {days_of_the_week[day_num_input]}!\n")
+
+#--------------------------------------------------------------
+
+
+# Division calculator error handling utilizing functions
+
+# def main():
+#     while True:
+#         try:
+#             num1 = float(input("Enter Numerator: "))
+#         except ValueError:
+#             print("Value Error: Enter an integer!\n")
+#         else:
+#             if num1 == 0:
+#                     print("Zero value is not acceptable! Retry.\n")
+#             else:
+#                 break
+      
+#     while True:
+#         try:
+#             num2 = float(input("Enter Denominator: "))
+#         except ValueError:
+#             print("Value Error: Enter an integer!\n")
+#         else:
+#             if num2 == 0:
+#                     print("Zero value is not acceptable! Retry.\n")
+#             else:
+#                 break     
+#     operation(num1, num2)    
+    
+# def operation(numerator, denominator):
+    
+#     result = numerator / denominator
+    
+#     if result % 1 == 0:
+#         print(f"Result: {int(result)}")
+#     else:
+#         print(f"Result: {result:.2f}")
+        
+# main()
+
+#-------------------------------------------------------------- 
