5b33c7532e7199a1d2de7fbc59c88492f33d5296
judelang18 <andradejude.github.repository@gmail.com>
2025-06-21 12:38:05 +0800

    update

--- python/Tricky_Logic.py
+++ python/Tricky_Logic.py
@@ -3,12 +3,22 @@
 
 # Nakagamit gyud og Chat GPT :(
     
-x = int(3)
-y = int(5)
+# x = int(3)
+# y = int(5)
 
-x, y = y, x # This is the operation to use if you opt to not use
-# any function or another variable as a place holder of any selected 
-# variable value to store in
+# x, y = y, x # This is the operation to use if you opt to not use
+# # any function or another variable as a place holder of any selected 
+# # variable value to store in
 
-print("Result of x:", x)
-print("Result of y:", y)
+# print("Result of x:", x)
+# print("Result of y:", y)
+
+x = 5
+y = 7
+temp = None
+temp = x
+x = y
+y = temp
+
+print(f"x = {x} and y = {y}")
+
