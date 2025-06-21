9fc70897bb58d7286864485c30ff9bbfdebfa35f
judelang18 <andradejude.github.repository@gmail.com>
2025-06-21 11:49:38 +0800

    update

---
+++ python/D11_importing_libraries.py
@@ -0,0 +1,35 @@
+#--------------------------------------------------------------
+# using a function from module random called choice wherein the system
+# randomize the sequence of values stored in a variable
+
+# import random
+
+# # this is a presentation sequence randomizer using shuffle function
+
+# presentors = ["Jude", "Johanna", "Paul", "Rhianzel"]
+# random.shuffle(presentors)
+
+# print("Presenters for today as of follows:")
+
+# # we will use for loop to iterate over the randomize sequence and print
+
+# for presentor in presentors:
+#     print(presentor)
+
+#--------------------------------------------------------------
+
+# Using choice function in random module to select a participant from
+# four students. We will specify the importing of module so that we
+# will just directly call the choice function having to not access library
+# like this random.choice() it'll be like this choice()
+
+# from random import choice
+
+# members = choice(["Jude", "Johanna", "Paul", "Rhianzel"])
+
+# print("Participant will be:")
+# print(members)
+
+
+#--------------------------------------------------------------
+
